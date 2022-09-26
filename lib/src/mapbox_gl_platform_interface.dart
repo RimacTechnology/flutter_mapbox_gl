a42a71aec50f192ddcfe0705edf882d1cb508208
mario.jerkovic <mario.jerkovic@rimac-technology.com>
2022-09-26 08:46:54 +0200

    feat: initial commit

---
+++ lib/src/mapbox_gl_platform_interface.dart
@@ -0,0 +1,21 @@
+library mapbox_gl_platform_interface;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+part 'platform_interface/callbacks.dart';
+part 'platform_interface/camera.dart';
+part 'platform_interface/circle.dart';
+part 'platform_interface/line.dart';
+part 'platform_interface/location.dart';
+part 'platform_interface/method_channel_mapbox_gl.dart';
+part 'platform_interface/symbol.dart';
+part 'platform_interface/fill.dart';
+part 'platform_interface/ui.dart';
+part 'platform_interface/mapbox_gl_platform_interface.dart';
+part 'platform_interface/source_properties.dart';
